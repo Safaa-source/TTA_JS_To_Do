607bf522133b58e0e95b15544492860936bf4d1d
Safaa-source <safay410@gmail.com>
2024-09-15 18:52:14 +0200

    feat:understanding properties and methodes

--- week12/version1.js
+++ week12/version1.js
@@ -1,7 +1,8 @@
 class Task{
-    constructor(title,description) {
+    constructor(title,description,category) {
         this.title = title;
         this.description = description;
+        this.category = category
         this.completed = false;
     }
 
@@ -11,21 +12,48 @@ class Task{
 
     displayDetails(){
         console.log('+-------------------------------------------------+')
+        console.log(`\tCaregory: ${this.category}`)
         console.log(`\tTask: ${this.title}`)
         console.log(`\tDescription: ${this.description}`)
         // console.log(`\tCompleted: ${this.completed}`)
-        if (this.completed) {
-            console.log('\tTask is completed')
-        }
-        else{
-            console.log('\tTask is not completed')
-        }
+        // if (this.completed) {
+        //     console.log('\tTask is completed')
+        // }
+        // else{
+        //     console.log('\tTask is not completed')
+        // }
+        console.log(`${this.completed ? "\tTask is completed" : "\tTask is not completed"}`);
         console.log('+-------------------------------------------------+')
+    }
+
+    // updateTask(newTitle, newDescription, newCategory){
+    //     this.title = newTitle
+    //     this.description = newDescription
+    //     this.category = newCategory
+    //     console.log('Task updated successfully')
+    // }
+    updateTaskTitle(newTitle){
+        this.title = newTitle
+        console.log('Task Tilte updated successfully')
+    }
+
+    updateTaskDescription(newDescription){
+        this.description = newDescription
+        console.log('Task Description updated successfully')
+    }
+
+    updateTaskDescription(newCaregory){
+        this.caregory = newCaregory
+        console.log('Task Caregory updated successfully')
     }
 }
 
-let task1 = new Task ('buy groceries' , 'get eggs, milk and bread form the store')
+let task1 = new Task ('buy groceries' , 'get eggs, milk and bread form the store' , 'shopping')
 
+task1.displayDetails()
+
+task1.updateTaskTitle('buy groceries - 1' , '1. get eggs, milk and bread form the store' , 'm1- shopping')
+console.log('\t Updated task details')
 task1.displayDetails()
 
 task1.toggleCompleted()
